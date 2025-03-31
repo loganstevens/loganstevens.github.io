3cebe9f11a23ec7e3c16e9c017d166eeae224cba
Logan <logancolestevens@gmail.com>
2025-03-31 00:40:39 -0400

    Update script.js - Time

--- js/script.js
+++ js/script.js
@@ -178,7 +178,7 @@ function updateViewCounter() {
 // Display last updated time
 function updateLastUpdatedTime() {
     // You can manually set this date or fetch it dynamically from a backend
-    const lastUpdated = 'March 30, 2025, 12:31 AM | EST';  // Replace with your actual update time
+    const lastUpdated = 'March 31, 2025, 12:40 AM | EST';  // Replace with your actual update time
     lastUpdatedElement.textContent = `Last Updated: ${lastUpdated}`;  // Display the last updated time
 }
 
