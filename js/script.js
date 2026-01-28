fc07a269bb9d5048d3206af19e19217d5e633d69
Logan <logancolestevens@gmail.com>
2026-01-27 20:12:00 -0500

    Update script.js - Time

--- js/script.js
+++ js/script.js
@@ -181,7 +181,7 @@ function updateViewCounter() {
 // Display last updated time
 function updateLastUpdatedTime() {
     // You can manually set this date or fetch it dynamically from a backend
-    const lastUpdated = 'January 27, 2026, 1:39 PM | EST';  // Replace with your actual update time
+    const lastUpdated = 'January 27, 2026, 8:11 PM | EST';  // Replace with your actual update time
     lastUpdatedElement.textContent = `Last Updated: ${lastUpdated}`;  // Display the last updated time
 }
 
@@ -281,6 +281,7 @@ romText.addEventListener('mouseover', function() {
 romText.addEventListener('mouseout', function() {
     this.textContent = "Elliot Huang";
 });
+
 
 
 
